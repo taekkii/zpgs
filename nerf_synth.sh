3026251cab87dfe0f303ebdcec5d24935a86d9ce
Jeongtaek Oh <ohjtgood@gmail.com>
2025-08-22 04:38:16 +0000

    requirements.txt fixed

--- nerf_synth.sh
+++ nerf_synth.sh
@@ -1,6 +1,6 @@
 # Launch main_train.py on every scene in nerf_synthetic=["./nerf_synthetic/mic","./nerf_synthetic/chair","./nerf_synthetic/ship","./nerf_synthetic/materials","./nerf_synthetic/lego","./nerf_synthetic/drums","./nerf_synthetic/ficus","./nerf_synthetic/hotdog"]
-dataset_path="./dataset/nerf_synthetic"
-for scene in chair drums ficus hotdog lego materials mic ship
+dataset_path="./ssd/nerf_data/nerf_synthetic"
+for scene in chair
 do
     scene_path="${dataset_path}/${scene}"
     ply_name="fused_light.ply"
